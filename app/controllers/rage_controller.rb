b4e6a4f6bc0948a6a9ec1fea61da27fca28f77a9
Kevin Wu <kevinwuhoo@gmail.com>
2011-04-01 10:57:31 -0700

    scrapes reddit

--- app/controllers/rage_controller.rb
+++ app/controllers/rage_controller.rb
@@ -1,4 +1,6 @@
 class RageController < ApplicationController
+
+  # Gets the most recent tweet by RageCurator
   def home
     require 'open-uri'
     require 'json'
@@ -15,7 +17,22 @@ class RageController < ApplicationController
   def queue
   end
 
+  # Scrapes reddit.com
   def scrape
+    require 'open-uri'
+    require 'nokogiri'
+    @comics = []
+    url = 'http://www.reddit.com/r/fffffffuuuuuuuuuuuu'
+    doc = Nokogiri::HTML(open(url))
+
+    doc.xpath('//a[@class="title "]').each do | method_span |  
+      @comics.push [method_span.content, method_span["href"]]
+    end  
+
+    doc.xpath('//a[@class="comments"]').each_with_index do | method_span, i |  
+      @comics[i].push method_span["href"]
+    end  
+    
   end
 
 end
